d48155bd2188713fb30bd39849ba10c67cc7c292
Mithun20522 <mithun20522@iiitd.ac.in>
2024-12-20 16:47:02 +0530

    minor fix

--- src/components/PatientDetailsPreview.tsx
+++ src/components/PatientDetailsPreview.tsx
@@ -15,27 +15,29 @@ export default function PatientDetailsPreview() {
   }
 
   return (
-    <div className="bg-gray-50 p-4 rounded-lg w-96">
-      <div className="space-y-2">
-        <div className="flex justify-between">
-          <span className="text-gray-500">Name:</span>
-          <span className="font-medium">
-            {basicDetails.firstName} {basicDetails.lastName}
-          </span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Age:</span>
-          <span className="font-medium">{basicDetails.age} years</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Mobile:</span>
-          <span className="font-medium">{basicDetails.mobileNo}</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Gender:</span>
-          <span className="font-medium">{basicDetails.gender}</span>
+    basicDetails.firstName && (
+      <div className="bg-gray-50 p-4 rounded-lg w-96">
+        <div className="space-y-2">
+          <div className="flex justify-between">
+            <span className="text-gray-500">Name:</span>
+            <span className="font-medium">
+              {basicDetails.firstName} {basicDetails.lastName}
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-gray-500">Age:</span>
+            <span className="font-medium">{basicDetails.age} years</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-gray-500">Mobile:</span>
+            <span className="font-medium">{basicDetails.mobileNo}</span>
+          </div>
+          <div className="flex justify-between">
+            <span className="text-gray-500">Gender:</span>
+            <span className="font-medium">{basicDetails.gender}</span>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
